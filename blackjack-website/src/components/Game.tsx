ff967013a98ede34b279e7f9f98524c306c7574a
Freddie Bergener <freddiembergener@gmail.com>
2021-06-07 18:45:24 -0400

    Added post deal display

--- blackjack-website/src/components/Game.tsx
+++ blackjack-website/src/components/Game.tsx
@@ -1,43 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { State } from "../redux/reducer";
-import UpChips from "./UpChips";
-import DownChips from "./DownChips";
+import PreDealDisplay from "./PreDealDisplay";
 
 // When the game starts, the first components that should be displayed are
 // the chips and the deal button
+// Once the cards are dealt, display the card and game button components
 
 const Game = () => {
-  const bankroll = useSelector((state: State) => state.bankroll);
   const pot = useSelector((state: State) => state.pot);
+  const bankroll = useSelector((state: State) => state.bankroll);
   const bet = useSelector((state: State) => state.bet);
-  const preDealDisplay = () => {
-    return (
-      <>
-        <h2>Bankroll: {bankroll.toLocaleString("en")}</h2>
-        {bankroll > 0 && pot === 0 ? (
-          <>
-            <h2>Increase Bet</h2> <UpChips />
-          </>
-        ) : (
-          ""
-        )}
-        {bet > 0 && pot === 0 ? (
-          <>
-            <h2>Lower Bet</h2> <DownChips />
-          </>
-        ) : (
-          ""
-        )}
-      </>
-    );
-  };
-
+  const splitPot = useSelector((state: State) => state.splitPot);
+  const playerHand = useSelector((state: State) => state.playerHand);
+  const splitHand = useSelector((state: State) => state.splitHand);
+  const dealerHand = useSelector((state: State) => state.dealerHand);
+  const playerCount = useSelector((state: State) => state.playerCount);
+  const splitCount = useSelector((state: State) => state.splitCount);
+  const dealerCount = useSelector((state: State) => state.dealerCount);
+  const hitBoolean = useSelector((state: State) => state.hitBoolean);
+  const stayBoolean = useSelector((state: State) => state.stayBoolean);
+  const doubleDownBoolean = useSelector(
+    (state: State) => state.doubleDownBoolean
+  );
+  const splitBoolean = useSelector((state: State) => state.splitBoolean);
+  const splitHitBoolean = useSelector((state: State) => state.splitHitBoolean);
+  const splitStayBoolean = useSelector(
+    (state: State) => state.splitStayBoolean
+  );
   return (
     <>
       <h1>Game</h1>
-      {preDealDisplay()}
+      {!pot ? PreDealDisplay() : ""}
       <Link to="/">
         <button>Back to Home</button>
       </Link>
---
+++ blackjack-website/src/components/PreDealDisplay.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { State } from "../redux/reducer";
+import UpChips from "./UpChips";
+import DownChips from "./DownChips";
+import { Deal } from "./GameButtons";
+
+const PreDealDisplay = () => {
+  const bankroll = useSelector((state: State) => state.bankroll);
+  const pot = useSelector((state: State) => state.pot);
+  const bet = useSelector((state: State) => state.bet);
+  return (
+    <>
+      <h2>Bankroll: {bankroll.toLocaleString("en")}</h2>
+      {bankroll > 0 && pot === 0 ? (
+        <>
+          <h2>Increase Bet</h2> <UpChips />
+        </>
+      ) : (
+        ""
+      )}
+      {bet > 0 && pot === 0 ? (
+        <>
+          <h2>Lower Bet</h2> <DownChips />
+        </>
+      ) : (
+        ""
+      )}
+      {Deal()}
+    </>
+  );
+};
+
+export default PreDealDisplay;
