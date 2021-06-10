d85e2c8c05293a4169afad2429d092ae92107840
Freddie Bergener <freddiembergener@gmail.com>
2021-06-10 16:23:48 -0400

    Cleaned up bankrupt component

--- blackjack-website/src/components/Bankrupt.tsx
+++ blackjack-website/src/components/Bankrupt.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { useDispatch } from "react-redux";
 
 const Bankrupt: React.FC = () => {
-  const dispatch = useDispatch();
   return (
     <>
       <h1>You ran out of chips</h1>
