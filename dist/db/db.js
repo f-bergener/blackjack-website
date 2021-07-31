55fef60707b00cd36c7a6d08e3db6e849e939078
Freddie Bergener <freddiembergener@gmail.com>
2021-07-31 18:03:08 -0400

    Fixed no pg_hba.conf error

--- dist/db/db.js
+++ dist/db/db.js
@@ -3,5 +3,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sequelize = require("sequelize");
 var pkgJSON = require("../../package.json");
 var databaseName = pkgJSON.name;
-var db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/" + databaseName);
+var config = {};
+if (process.env.DATABASE_URL) {
+    config = {
+        logging: false,
+        dialectOptions: {
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        },
+    };
+}
+var db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/" + databaseName, config);
 module.exports = db;
--- src/db/db.ts
+++ src/db/db.ts
@@ -3,8 +3,22 @@ const pkgJSON = require("../../package.json");
 
 const databaseName = pkgJSON.name;
 
+let config = {};
+
+if (process.env.DATABASE_URL) {
+  config = {
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  };
+}
+
 const db = new Sequelize(
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
 );
 
 module.exports = db;
