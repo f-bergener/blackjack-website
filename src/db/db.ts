62d4aba17ac829a6eaf8f4ff2484561e5fa9051c
Freddie Bergener <freddiembergener@gmail.com>
2021-07-31 17:03:33 -0400

    Fixed database URL

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,7 +3,9 @@ const pkgJSON = require("../../package.json");
 
 const databaseName = pkgJSON.name;
 
-const db = new Sequelize(`postgres://localhost:5432/${databaseName}`);
+const db = new Sequelize(
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`
+);
 
 module.exports = db;
 
