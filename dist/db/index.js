8b0fe40f68044bd3a07947733011b23f92821ac8
Freddie Bergener <57042252+f-bergener@users.noreply.github.com>
2021-08-19 22:48:25 -0400

    Delete dist directory

--- dist/db/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var db = require("./db");
-var User = require("./users");
-module.exports = {
-    db: db,
-    User: User,
-};
